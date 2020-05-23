375a5df3773c3b165bf038d12679fe0c69b55be5
sashasimulik <simik9697@gmail.com>
2020-05-23 22:52:22 +0300

    Добавьте файлы проекта.

---
+++ OOPLab2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOPLab2._2
+{
+    public class Program
+    {
+        static public double Calculation(double x)
+        {           
+            double z = Math.Pow(4.0, -Math.Cos(x));
+            return z;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Результати обчислення функцiї y = f(x) на промiжку вiд 0 до 3 з кроком dx = 0.05.\n\tx\t\ty = f(x)");
+            double x = 0;
+            double y;
+            while (x <= 3)
+            {
+                y = Calculation(x);
+                Console.WriteLine("\t{0}\t\t{1}", Math.Round(x, 2), y);
+                x += 0.05;
+            }
+        }
+    }
+}
